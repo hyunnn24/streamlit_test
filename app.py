deec692d7f8e85756a2ee8d75584e7a18445e3db
hyunnn24 <snow3515@naver.com>
2024-05-29 07:40:28 +0000

    dkfasdf

--- app.py
+++ app.py
@@ -1,22 +1,44 @@
 import streamlit as st
-
-st.header("API Key 를 입력하세요")
-API=st.text_input("API", type="password")
-
-st.header("무엇이든 물어보세요.")
-prompt = st.text_input("질문?")
-st.session_state.API
-
-
 from openai import OpenAI
-client = OpenAI(api_key=API)
-response = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": prompt}
-  ]
-)
-print(response.choices[0].message.content)
-with st.chat_message("AI"):
-    st.write(response.choices[0].message.content)
+
+
+def APIINPUT():
+    st.header("API Key 를 입력하세요")
+    API = st.text_input("API", type="password")
+    if API:
+        st.session_state.API = API
+
+def chating():
+    st.header("무엇이든 물어보세요.")
+    prompt = st.text_input("질문?")
+    
+    if prompt and 'API' in st.session_state:
+        client = OpenAI(api_key=st.session_state.API)
+        response = client.chat_completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        with st.chat_message("AI"):
+            st.write(response.choices[0].message.content)
+    elif not prompt:
+        st.write("질문을 입력하세요.")
+    else:
+        st.write("API Key를 먼저 입력하세요.")
+
+def drawing():
+    st.header("그림을 그리는 페이지")
+  
+
+
+page = st.sidebar.selectbox("페이지 선택", ["API", "챗봇", "그림"])
+
+
+if page == "API":
+    APIINPUT()
+elif page == "챗봇":
+    chating()
+elif page == "그림":
+    drawing()
