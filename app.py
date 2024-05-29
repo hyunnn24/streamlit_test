fb13202b4724e8a376fdf1643398d8dff5ad246b
hyunnn24 <snow3515@naver.com>
2024-05-29 07:29:16 +0000

    def

--- app.py
+++ app.py
@@ -2,21 +2,26 @@ import streamlit as st
 
 st.header("API Key 를 입력하세요")
 API=st.text_input("API", type="password")
-
-st.header("무엇이든 물어보세요.")
-prompt = st.text_input("질문?")
 st.session_state.API
 
+def chating():
+  st.header("무엇이든 물어보세요.")
+  prompt = st.text_input("질문?")
+  
 
-from openai import OpenAI
-client = OpenAI(api_key=API)
-response = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": prompt}
-  ]
-)
-print(response.choices[0].message.content)
-with st.chat_message("AI"):
-    st.write(response.choices[0].message.content)
+
+  from openai import OpenAI
+  client = OpenAI(api_key=API)
+  response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "You are a helpful assistant."},
+      {"role": "user", "content": prompt}
+    ]
+  )
+  print(response.choices[0].message.content)
+  with st.chat_message("AI"):
+      st.write(response.choices[0].message.content)
+
+if page == "챗봇":
+    chating()
