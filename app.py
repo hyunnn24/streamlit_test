ab2fc892d28d8efc0a915d436ea09bd7a728f260
hyunnn24 <snow3515@naver.com>
2024-05-29 07:02:54 +0000

    asdf

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import streamlit as st
 
-pip install -q streamlit openai
-npm install localtunnel
-
-import streamlit as st
-
 st.header("API Key 를 입력하세요")
 API = st.text_input("API KEY?")
-st.text_input("API_KEY", key=API)
-
+st.text_input("API_keys", key=API)
 
 st.header("무엇이든 물어보세요.")
 prompt = st.text_input("질문?")
@@ -25,6 +19,3 @@ response = client.chat.completions.create(
 print(response.choices[0].message.content)
 with st.chat_message("AI"):
     st.write(response.choices[0].message.content)
-
-st.header("무엇이든 그려보세요.")
-pprompt = st.text_input("프롬프트?")
