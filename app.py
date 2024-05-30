6e4261e3ae9278ea7d4c2a355ba99a7e18b49809
hyunnn24 <snow3515@naver.com>
2024-05-30 07:04:16 +0000

    ㅋㅋㅋ

--- app.py
+++ app.py
@@ -41,7 +41,24 @@ def drawing():
     st.image(image_url)
 
 def chatting():
+    st.header("무엇이든 물어보세요.")
+    prompt = st.text_input("질문?")
     
+    if prompt and 'API' in st.session_state:
+        client = OpenAI(api_key=st.session_state.API)
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        with st.chat_message("AI"):
+            st.write(response.choices[0].message.content)
+    elif not prompt:
+        st.write("질문을 입력하세요.")
+    else:
+        st.write("API Key를 먼저 입력하세요.")
   
 
 
