ba3cafce63e89ad55d6f93691025b6069419fd72
hyunnn24 <snow3515@naver.com>
2024-05-30 07:12:15 +0000

    챗

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 import streamlit as st
 from openai import OpenAI
 
+import time
+
+def run_and_wait(client, assistant, thread):
+  run = client.beta.threads.runs.create(
+    thread_id=thread.id,
+    assistant_id=assistant.id
+  )
+  while True:
+    run_check = client.beta.threads.runs.retrieve(
+      thread_id=thread.id,
+      run_id=run.id
+    )
+    print(run_check.status)
+    if run_check.status in ['queued','in_progress']:
+      time.sleep(2)
+    else:
+      break
+  return run
 
 def APIINPUT():
     st.header("API Key 를 입력하세요")
@@ -28,6 +46,28 @@ def chating():
     else:
         st.write("API Key를 먼저 입력하세요.")
 
+assistant = client.beta.assistants.create(
+  name="streamlit",
+  description="you are an helpful assistant",
+  model="gpt-4o",
+  tools=[{"type": "code_interpreter"}]
+)
+
+thread = client.beta.threads.create(
+  messages=[
+    {
+      "role": "user",
+      "content": userinput
+      
+    }
+  ]
+)
+
+
+
+
+
+
 def drawing():
     
     st.header("무엇이든 그려보세요.")
@@ -41,25 +81,10 @@ def drawing():
     st.image(image_url)
 
 def chatting():
-    st.header("무엇이든 물어보세요.")
-    prompt = st.text_input("질문?")
-    
-    if prompt and 'API' in st.session_state:
-        client = OpenAI(api_key=st.session_state.API)
-        response = client.chat.completions.create(
-            model="gpt-4o",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": prompt}
-            ]
-        )
-        with st.chat_message("AI"):
-            st.write(response.choices[0].message.content)
-    elif not prompt:
-        st.write("질문을 입력하세요.")
-    else:
-        st.write("API Key를 먼저 입력하세요.")
-  
+    run = run_and_wait(client, assistant, thread)
+    thread_messages = client.beta.threads.messages.list(thread.id)
+    for msg in thread_messages.data:
+    print(f"{msg.role}: {msg.content[0]}")
 
 
 page = st.sidebar.selectbox("페이지 선택", ["API", "챗봇", "그림","Chat"])
