996854aa7b270fa904ae410952282f168fe5754e
hyunnn24 <snow3515@naver.com>
2024-05-30 07:19:07 +0000

    갈엎

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 import streamlit as st
 from openai import OpenAI
-
 import time
 
 def run_and_wait(client, assistant, thread):
-  run = client.beta.threads.runs.create(
-    thread_id=thread.id,
-    assistant_id=assistant.id
-  )
-  while True:
-    run_check = client.beta.threads.runs.retrieve(
-      thread_id=thread.id,
-      run_id=run.id
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id
     )
-    print(run_check.status)
-    if run_check.status in ['queued','in_progress']:
-      time.sleep(2)
-    else:
-      break
-  return run
+    while True:
+        run_check = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id
+        )
+        print(run_check.status)
+        if run_check.status in ['queued', 'in_progress']:
+            time.sleep(2)
+        else:
+            break
+    return run
 
 def APIINPUT():
     st.header("API Key 를 입력하세요")
@@ -46,49 +45,43 @@ def chating():
     else:
         st.write("API Key를 먼저 입력하세요.")
 
-assistant = client.chat.assistants.create(
-    name="streamlit",
-    description="you are an helpful assistant",
-    model="gpt-4o",
-    tools=[{"type": "code_interpreter"}]
-)
-
-thread = client.beta.threads.create(
-    messages=[
-    {
-      "role": "user",
-      "content": userinput
-      
-    }
-    ]
-)
-
-
-
-
-
-
 def drawing():
-    
     st.header("무엇이든 그려보세요.")
     pprompt = st.text_input("프롬프트?")
-
-    from openai import OpenAI
-    client = OpenAI(api_key=st.session_state.API)
-    response = client.images.generate(model="dall-e-3",prompt=pprompt)
-    image_url = response.data[0].url
     
-    st.image(image_url)
+    if 'API' in st.session_state:
+        client = OpenAI(api_key=st.session_state.API)
+        response = client.images.generate(model="dall-e-3", prompt=pprompt)
+        image_url = response.data[0].url
+        st.image(image_url)
+    else:
+        st.write("API Key를 먼저 입력하세요.")
 
 def chatting():
-    run = run_and_wait(client, assistant, thread)
-    thread_messages = client.beta.threads.messages.list(thread.id)
-    for msg in thread_messages.data:
-        print(f"{msg.role}: {msg.content[0]}")
+    if 'API' in st.session_state:
+        client = OpenAI(api_key=st.session_state.API)
+        assistant = client.chat.assistants.create(
+            name="streamlit",
+            description="you are an helpful assistant",
+            model="gpt-4o",
+            tools=[{"type": "code_interpreter"}]
+        )
+        thread = client.beta.threads.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": st.text_input("유저 입력")
+                }
+            ]
+        )
+        run = run_and_wait(client, assistant, thread)
+        thread_messages = client.beta.threads.messages.list(thread.id)
+        for msg in thread_messages.data:
+            print(f"{msg.role}: {msg.content[0]}")
+    else:
+        st.write("API Key를 먼저 입력하세요.")
 
-
-page = st.sidebar.selectbox("페이지 선택", ["API", "챗봇", "그림","Chat"])
-
+page = st.sidebar.selectbox("페이지 선택", ["API", "챗봇", "그림", "Chat"])
 
 if page == "API":
     APIINPUT()
